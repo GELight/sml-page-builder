e82fc22f232eed00dfbf74268310a8d1c0124090
Mario Linz <gelight@gmx.de>
2021-04-13 20:56:37 +0200

    Test with typescript type definitions

--- dist/index.js
+++ dist/index.js
@@ -3,16 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.SmlToHtmlBuilder = exports.SmlPageBuilder = exports.CustomTagText = exports.CustomTagIncludeGithubMarkdownFile = exports.CustomTag = void 0;
 const CustomTag_1 = __importDefault(require("./builder/CustomTag"));
+exports.CustomTag = CustomTag_1.default;
 const CustomTagIncludeGithubMarkdownFile_1 = __importDefault(require("./builder/CustomTagIncludeGithubMarkdownFile"));
+exports.CustomTagIncludeGithubMarkdownFile = CustomTagIncludeGithubMarkdownFile_1.default;
 const CustomTagText_1 = __importDefault(require("./builder/CustomTagText"));
+exports.CustomTagText = CustomTagText_1.default;
 const SmlPageBuilder_1 = __importDefault(require("./builder/SmlPageBuilder"));
+exports.SmlPageBuilder = SmlPageBuilder_1.default;
 const SmlToHtmlBuilder_1 = __importDefault(require("./builder/SmlToHtmlBuilder"));
-exports.default = {
-    CustomTag: CustomTag_1.default,
-    CustomTagIncludeGithubMarkdownFile: CustomTagIncludeGithubMarkdownFile_1.default,
-    CustomTagText: CustomTagText_1.default,
-    SmlPageBuilder: SmlPageBuilder_1.default,
-    SmlToHtmlBuilder: SmlToHtmlBuilder_1.default
-};
+exports.SmlToHtmlBuilder = SmlToHtmlBuilder_1.default;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import CustomTagText from "./builder/CustomTagText";
 import SmlPageBuilder from "./builder/SmlPageBuilder";
 import SmlToHtmlBuilder from "./builder/SmlToHtmlBuilder";
 
-export default {
+export {
     CustomTag,
 
     CustomTagIncludeGithubMarkdownFile,
